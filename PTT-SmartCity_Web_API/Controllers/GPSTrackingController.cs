001c89033d8fabf2fa3978175eb54e4d93dfdb3e
atthachai2212 <attachai.thb@gmail.com>
2020-02-13 14:06:48 +0700

    Add GPSTrackingController

---
+++ PTT-SmartCity_Web_API/Controllers/GPSTrackingController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using PTT_SmartCity_Web_API.Models;
+using PTT_SmartCity_Web_API.Services;
+
+namespace PTT_SmartCity_Web_API.Controllers
+{
+    public class GPSTrackingController : ApiController
+    {
+        private dbBusTrackingContext db = new dbBusTrackingContext();
+
+        // GET: api/GPSTracking
+        [Route("api/gpsTracking")]
+        public IQueryable<tbGPS_Realtime> GettbGPS_Realtime()
+        {
+            return db.tbGPS_Realtime;
+        }
+
+        // GET: api/GPSTracking/5
+        [Route("api/gpsTracking")]
+        [ResponseType(typeof(tbGPS_Realtime))]
+        public async Task<IHttpActionResult> GettbGPS_Realtime(DateTime id)
+        {
+            tbGPS_Realtime tbGPS_Realtime = await db.tbGPS_Realtime.FindAsync(id);
+            if (tbGPS_Realtime == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(tbGPS_Realtime);
+        }
+
+        // PUT: api/GPSTracking/5
+        [Route("api/gpsTracking")]
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PuttbGPS_Realtime(DateTime id, tbGPS_Realtime tbGPS_Realtime)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != tbGPS_Realtime.Date)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(tbGPS_Realtime).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!tbGPS_RealtimeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/GPSTracking
+        [Route("api/gpsTracking")]
+        [ResponseType(typeof(tbGPS_Realtime))]
+        public async Task<IHttpActionResult> PosttbGPS_Realtime(tbGPS_Realtime tbGPS_Realtime)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.GetErrorModelState());
+            }
+
+            db.tbGPS_Realtime.Add(tbGPS_Realtime);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (tbGPS_RealtimeExists(tbGPS_Realtime.Date))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = tbGPS_Realtime.Date }, tbGPS_Realtime);
+        }
+
+        // DELETE: api/GPSTracking/5
+        [Route("api/gpsTracking")]
+        [ResponseType(typeof(tbGPS_Realtime))]
+        public async Task<IHttpActionResult> DeletetbGPS_Realtime(DateTime id)
+        {
+            tbGPS_Realtime tbGPS_Realtime = await db.tbGPS_Realtime.FindAsync(id);
+            if (tbGPS_Realtime == null)
+            {
+                return NotFound();
+            }
+
+            db.tbGPS_Realtime.Remove(tbGPS_Realtime);
+            await db.SaveChangesAsync();
+
+            return Ok(tbGPS_Realtime);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool tbGPS_RealtimeExists(DateTime id)
+        {
+            return db.tbGPS_Realtime.Count(e => e.Date == id) > 0;
+        }
+    }
+}
